4610b614ee08ce5ea36e3c7ba9f9326d44ac049d
Sarah King <009kings@gmail.com>
2018-11-25 22:52:26 -0800

    created game logic

--- Memeory Game/js/main.js
+++ Memeory Game/js/main.js
@@ -1,10 +1,31 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "Queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png"
+    },
+    {
+        rank: "Queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png"
+    },
+    {
+        rank: "King",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png"
+    },
+    {
+        rank: "King",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png"
+    }
+];
 var cardsInPlay = [];
 
 var checkForMatch = function() {
-    if (cardsInPlay[0] === cardsInPlay[1]) {
+    if (cardsInPlay[0].rank === cardsInPlay[1].rank) {
         console.log("You found a match!")
     } else {
         console.log("Sorry, try again.")
@@ -12,7 +33,7 @@ var checkForMatch = function() {
 }
 
 var flipCard = function(cardId) {
-    console.log("User flipped " + cards[cardId]);
+    console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId]);
     if (cardsInPlay.length === 2) {
         checkForMatch();
@@ -21,8 +42,12 @@ var flipCard = function(cardId) {
 flipCard(0);
 flipCard(2);
 
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+if (cardsInPlay.length === 2 && cardsInPlay[0].rank === cardsInPlay[1].rank) {
     alert("You found a match!");
 } else if (cardsInPlay.length === 2) {
     alert("No dice. Try again.");
 }
+
+console.log("flipped cards were of the suits " + cardsInPlay[0].suit + " and " + cardsInPlay[1].suit);
+console.log(cardsInPlay[0].cardImage);
+console.log(cardsInPlay[1].cardImage);
