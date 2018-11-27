f4af24d6d23724a9f6fb8b89ad8376c62853c823
009kings <009kings@gmail.com>
2018-11-26 19:26:34 -0800

    fixed card with sweet hack from Lemon

--- Memeory Game/js/main.js
+++ Memeory Game/js/main.js
@@ -30,22 +30,34 @@ var checkForMatch = function() {
     }
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+    var cardId = this.getAttribute("data-id");
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId]);
     if (cardsInPlay.length === 2) {
         checkForMatch();
     }
 }
-flipCard(0);
-flipCard(2);
 
 if (cardsInPlay.length === 2 && cardsInPlay[0].rank === cardsInPlay[1].rank) {
     alert("You found a match!");
 } else if (cardsInPlay.length === 2) {
     alert("No dice. Try again.");
 }
+//added After theDOM
 
-console.log("flipped cards were of the suits " + cardsInPlay[0].suit + " and " + cardsInPlay[1].suit);
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        var cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+    }
+};
+createBoard();
+
+
+/*console.log("flipped cards were of the suits " + cardsInPlay[0].suit + " and " + cardsInPlay[1].suit);
 console.log(cardsInPlay[0].cardImage);
-console.log(cardsInPlay[1].cardImage);
+console.log(cardsInPlay[1].cardImage);*/
